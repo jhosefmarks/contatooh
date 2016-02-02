b0a122156ec666d95b5fa819bd6a71f3d8d77fd8
Jhosef Marks <jhosef@gmail.com>
2016-02-02 16:31:55 -0200

    Correção arquivo do Travis

--- config/protractor.js
+++ config/protractor.js
@@ -16,7 +16,7 @@ exports.config = {
     onPrepare: function () {
         'use strict';
 
-        browser.get('http://localhost:3000').then(function () {
+        browser.get('http://localhost:3000/#/auth').then(function () {
             browser.driver.findElement(by.id('entrar')).click();
             browser.driver.findElement(by.id('login_field')).sendKeys(config.seleniumUser);
             browser.driver.findElement(by.id('password')).sendKeys(config.seleniumUserPassword);
